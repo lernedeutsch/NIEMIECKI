b87679561355a0e60490b3f3105a35184c2ff8af
deutsch.lernen <moni.s.w@o2.pl>
2026-09-22 11:08:18 +0200

    Connect NIEMIECKI to Nele2 backend

--- niemiecki/config/app-config.js
+++ niemiecki/config/app-config.js
@@ -1,1 +1,15 @@
+/**
+ * Konfiguracja aplikacji NIEMIECKI
+ * Backend: Nele2
+ */
+const appConfig = {
+  api: {
+    baseUrl: "https://nele2-backend.onrender.com",
+    timeout: 70000
+  }
+};
 
+if (typeof window !== "undefined") {
+  window.appConfig = appConfig;
+  window.NELE_API_BASE = appConfig.api.baseUrl;
+}
